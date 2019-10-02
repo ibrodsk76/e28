beee960acc11f1328442b0c00637bc384f2b2df3
ibrodsk76 <47118722+ibrodsk76@users.noreply.github.com>
2019-10-02 14:25:35 -0400

    Project 1 progress

--- p1/script.js
+++ p1/script.js
@@ -2,9 +2,8 @@ let app = new Vue({
     el: '#app',
     data: {
         'numberOfPlayers': null,
-        'players': ['',''],
-        'currentPlayer': '',
-        'currentSymbol': 'X',
+        'players': [{name:'', symbol:'X'},{name:'',symbol:'O'}],
+        'currentPlayer': 0,
         'gameOver': false,
         'GameResults': null,
         'gameTable': [['','',''],
@@ -14,41 +13,44 @@ let app = new Vue({
     },
     methods: {
         makeMove: function(rowindex,cellindex) {
+            // don't do anything if a cell is already occupied or game is over
             if (this.gameTable[rowindex][cellindex] != "" || this.gameOver)
                 return;
-            if (this.numberOfPlayers == 1 && !this.players[1])
-                this.players[1] = 'Computer';
-            this.gameTable[rowindex].splice(cellindex,1,this.currentSymbol);
-            if (this.isGameOver())
+            // if first time here and one player is playing, second player's name is "Computer"
+            if (this.numberOfPlayers == 1 && !this.players[1].name)
+                this.players[1].name = 'Computer';
+            // insert the symbol of the current playeer
+            this.gameTable[rowindex].splice(cellindex,1,this.players[this.currentPlayer].symbol);
+            let result = this.isGameOver(this.gameTable, this.players[this.currentPlayer].symbol)
+            if (result == 2)
             {
+                this.GameResults = this.players[this.currentPlayer].name + " won!";   
                 gameOver = true;
                 return;
             }
-            if (this.currentSymbol == "X") 
-                this.currentSymbol = "O";
-            else
-                this.currentSymbol ="X";
+            else if (result == 1)
+            {
+                this.GameResults = "It's a draw!";    
+                gameOver = true;
+                return;
+            }
             
-            if (this.currentPlayer == this.players[0])
-                this.currentPlayer = this.players[1];
-            else
-                this.currentPlayer = this.players[0];    
+            this.currentPlayer = (this.currentPlayer == 0) ? 1 : 0;
 
             if (this.numberOfPlayers == 1)    
             {
                 // Computer makes a move
             }
         },
-        isGameOver: function() {
+        isGameOver: function(MyTable, symbol) {
             // checking rows
             for (var i = 0; i < 3; i++) {
                 for (var j = 0; j < 3; j++)
                 {
-                    if (this.gameTable[i][j] != this.currentSymbol)
+                    if (MyTable[i][j] != symbol)
                         break;
                     if (j == 2) // if at the end of the row, then the current player won
-                    {
-                        this.GameResults = this.currentPlayer + " won!";    
+                    {                         
                         return 2;
                     }
                 }
@@ -57,33 +59,30 @@ let app = new Vue({
             for (var j = 0; j < 3; j++) {
                 for (var i = 0; i < 3; i++)
                 {
-                    if (this.gameTable[i][j] != this.currentSymbol)
+                    if (MyTable[i][j] != symbol)
                         break;
                     if (i == 2) // if at the bottom of the column, then the current player won
                     {
-                        this.GameResults = this.currentPlayer + " won!";    
                         return 2;
                     }
                 }
             }
             // checking diagonal top left to bottom right
             for (var i = 0; i < 3; i++) {
-                if (this.gameTable[i][i] != this.currentSymbol)
+                if (MyTable[i][i] != symbol)
                     break;
                 if (i == 2)
                 {
-                    this.GameResults = this.currentPlayer + " won!";    
                     return 2;
                 }
             }
             // checking diagonal top right to bottom left
             for (var i = 0; i < 3; i++) {
                 j = 2-i;
-                if (this.gameTable[i][j] !=this.currentSymbol)
+                if (MyTable[i][j] != symbol)
                     break;
                 if (i == 2)
                 {
-                    this.GameResults = this.currentPlayer + " won!";    
                     return 2;
                 }
             }
@@ -91,11 +90,10 @@ let app = new Vue({
             for (var i = 0; i < 3; i++) {
                 for (var j = 0; j < 3; j++)
                 {
-                    if (this.gameTable[i][j] == "")
+                    if (MyTable[i][j] == "")
                         return 0;
                     if (i == 2 && j == 2)
                     {
-                        this.GameResults = "It's a draw!";    
                         return 1;
                     }
                 }
@@ -105,9 +103,8 @@ let app = new Vue({
         // reseting back to beginning of the game, all cells empty
         onReset: function() {
             this.numberOfPlayers = null;
-            this.players =  ['',''];
-            this.currentPlayer = '';
-            this.currentSymbol = 'X';
+            this.players = [{name:'', symbol:'X'},{name:'',symbol:'O'}],
+            this.currentPlayer = 0;
             this.gameOver = false;
             this.GameResults = null;
             this.gameTable = [['','',''],
