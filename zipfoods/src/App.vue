7b796f04d21e9a791d74a0105dee8bcc37ae5220
Irene Brodskiy <ibrodsk@yahoo.com>
2019-11-06 19:19:20 -0500

    =week 9 zipfoods example

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img id="logo" alt="ZipFoods logo" src='./assets/images/zipfoods-logo.png'>
+  
+    <nav>
+      <ul>
+        <li v-for='link in links' :key='link'>
+            <router-link exact :to='paths[link]'>{{ link }}</router-link>
+        </li>
+      </ul>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      links: ['home', 'products', 'categories'],
+      paths: {
+        home: '/',
+        products: '/products',
+        categories: '/categories'
+      }
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+@import './assets/css/zipfoods.scss';
+</style>
